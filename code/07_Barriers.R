ae3c065c63fd9c8667fc78e287931e740796296e
ameztegui <ameztegui@gmail.com>
2018-01-30 08:53:56 +0100

    Added bump chart

--- code/07_Barriers.R
+++ code/07_Barriers.R
@@ -35,7 +35,7 @@ library(tidyverse)
             dplyr::summarise(score = mean(score, na.rm=T)) %>%
         group_by(Province) %>%
             dplyr::mutate(rank = dense_rank(-score))%>%
-        mutate(Barriers = mf_labeller(Barrier),
+        mutate(Barriers = mf_labeller_bump(Barrier),
                Province = fct_recode(Province, 
                                      "British \n Columbia" = "British Columbia",
                                      "New\nBrunswick" = "New Brunswick"))
@@ -46,7 +46,7 @@ library(tidyverse)
         dplyr::summarise(score = mean(score, na.rm=T)) %>%
         group_by(Stakeholder) %>%
         dplyr::mutate(rank = dense_rank(-score)) %>%
-        mutate(Barriers = mf_labeller(Barrier),
+        mutate(Barriers = mf_labeller_bump(Barrier),
                Stakeholder = fct_recode(Stakeholder, 
                                       "Industry" = "Industry",
                                       "Provincial\nGovernment" = "Provincial Gov.",
@@ -60,15 +60,15 @@ library(tidyverse)
 
     # Per province
     
-    pdf("./figs/Barriers_Bump_Province.pdf", width=27, height=16)
+    pdf("./figs/Barriers_Bump_Province.pdf", width=32, height=22)
         p <- ggplot(barriers_prov, aes(Province, rank,
                                   group = Barriers, 
                                   colour = fct_reorder2(Barriers, Province, -rank), 
                                   label = Barriers)) + 
-            geom_line(size=3.5) + 
+            geom_line(size=4) + 
             geom_text(data = subset(barriers_prov,Province == "New\nBrunswick"), 
-                      size=10, aes(x = Province, hjust = -0.1), lineheight=0.85) + 
-            geom_point(size=8) +
+                      size=11, aes(x = Province, hjust = -0.1), lineheight=0.85, color ="black") + 
+            geom_point(size=8.5) +
             theme_bw() + 
             theme(legend.position = "none", 
                   panel.border = element_blank(),
@@ -77,8 +77,8 @@ library(tidyverse)
             scale_x_discrete(breaks = c(levels(barriers_prov$Province)), "") + 
             scale_y_continuous(breaks = NULL,trans = "reverse") +
             xlab(NULL) + ylab(NULL) +
-            theme(axis.text=element_text(size=32),
-                  plot.margin = unit(c(0,22,0.5,0), "cm"))
+            theme(axis.text=element_text(size=34),
+                  plot.margin = unit(c(0.5,22,0.5,0.5), "cm"))
         
         # Code to override clipping
         gt <- ggplot_gtable(ggplot_build(p))
@@ -87,15 +87,15 @@ library(tidyverse)
     dev.off()
 
     # Per stakeholder
-    pdf("./figs/Barriers_Bump_Stakeholder.pdf", width=27, height=16)
-        p <- ggplot(barriers_stake, aes(Stakeholder, rank,
+    pdf("./figs/Barriers_Bump_Stakeholder.pdf", width=32, height=22)
+        q <- ggplot(barriers_stake, aes(Stakeholder, rank,
                                        group = Barriers, 
                                        colour = fct_reorder2(Barriers, Stakeholder, -rank), 
                                        label = Barriers)) + 
-            geom_line(size=3.5) + 
-            geom_point(size=8) +
+            geom_line(size=4) + 
+            geom_point(size=8.5) +
             geom_text(data = subset(barriers_stake,Stakeholder == "Students"), 
-                      size=10, aes(x = Stakeholder, hjust = -0.1), lineheight=0.85) + 
+                      size=11, aes(x = Stakeholder, hjust = -0.1), lineheight=0.85, color = "black") + 
             theme_bw() + 
             theme(legend.position = "none", 
                   panel.border = element_blank(),
@@ -104,47 +104,47 @@ library(tidyverse)
             scale_x_discrete(breaks = c(levels(barriers_stake$Stakeholder), "")) + 
             scale_y_continuous(breaks = NULL,trans = "reverse") +
             xlab(NULL) + ylab(NULL) +
-            theme(axis.text=element_text(size=32),
-                  plot.margin = unit(c(0,22,0.5,0), "cm"))
+            theme(axis.text=element_text(size=34),
+                  plot.margin = unit(c(0.5,22,0.5,0.5), "cm"))
         
         # Code to override clipping
-        gt <- ggplot_gtable(ggplot_build(p))
+        gt <- ggplot_gtable(ggplot_build(q))
         gt$layout$clip[gt$layout$name == "panel"] <- "off"
         grid.draw(gt)
     dev.off()
 
 # ANOVA for barriers to implementation --------------------------------------------
-    
-    survey_comparison(survey,colnames(survey)[49],"Province")
-    survey_comparison(survey,colnames(survey)[50],"Province")
-    survey_comparison(survey,colnames(survey)[51],"Province")
-    survey_comparison(survey,colnames(survey)[52],"Province")
-    survey_comparison(survey,colnames(survey)[53],"Province")
-    survey_comparison(survey,colnames(survey)[54],"Province")
-    survey_comparison(survey,colnames(survey)[55],"Province")
-    survey_comparison(survey,colnames(survey)[56],"Province")
-    survey_comparison(survey,colnames(survey)[57],"Province")
-    survey_comparison(survey,colnames(survey)[58],"Province")
-    survey_comparison(survey,colnames(survey)[59],"Province")
-    survey_comparison(survey,colnames(survey)[60],"Province")
-    survey_comparison(survey,colnames(survey)[61],"Province")
-    survey_comparison(survey,colnames(survey)[62],"Province")
-    survey_comparison(survey,colnames(survey)[63],"Province")
-
-    survey_comparison(survey,colnames(survey)[49],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[50],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[51],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[52],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[53],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[54],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[55],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[56],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[57],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[58],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[59],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[60],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[61],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[62],"Stakeholder")
-    survey_comparison(survey,colnames(survey)[63],"Stakeholder")
+    # 
+    # survey_comparison(survey,colnames(survey)[49],"Province")
+    # survey_comparison(survey,colnames(survey)[50],"Province")
+    # survey_comparison(survey,colnames(survey)[51],"Province")
+    # survey_comparison(survey,colnames(survey)[52],"Province")
+    # survey_comparison(survey,colnames(survey)[53],"Province")
+    # survey_comparison(survey,colnames(survey)[54],"Province")
+    # survey_comparison(survey,colnames(survey)[55],"Province")
+    # survey_comparison(survey,colnames(survey)[56],"Province")
+    # survey_comparison(survey,colnames(survey)[57],"Province")
+    # survey_comparison(survey,colnames(survey)[58],"Province")
+    # survey_comparison(survey,colnames(survey)[59],"Province")
+    # survey_comparison(survey,colnames(survey)[60],"Province")
+    # survey_comparison(survey,colnames(survey)[61],"Province")
+    # survey_comparison(survey,colnames(survey)[62],"Province")
+    # survey_comparison(survey,colnames(survey)[63],"Province")
+    # 
+    # survey_comparison(survey,colnames(survey)[49],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[50],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[51],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[52],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[53],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[54],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[55],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[56],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[57],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[58],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[59],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[60],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[61],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[62],"Stakeholder")
+    # survey_comparison(survey,colnames(survey)[63],"Stakeholder")
 
     
