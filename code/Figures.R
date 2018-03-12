532aa2178bea6c63d46d3705e7f9ceea825d7121
ameztegui <ameztegui@gmail.com>
2018-03-12 15:37:06 +0100

    New figure names, function to produce letters after KW test

--- code/Figures.R
+++ code/Figures.R
@@ -6,13 +6,13 @@ source("./code/02_Likert.R")
 
 
 
-# Figure 2: Overall Responses ---------------------------------------------
+# Figure 1: Overall Responses ---------------------------------------------
     
     # Change names of the likert scale so they can be displayed in two rows (only for lower pannel)
       colnames(likert_current$results) <- c("Item", "Strongly\n disagree", "Disagree","Slightly\n disagree",
                                "Neutral", "Slightly\n agree", "Agree", "Strongly\n agree")
 
-    Fig2a <- plot(likert_gen_imp, ordered=F,  wrap= 45,text.size=5) +
+    Fig1a <- plot(likert_gen_imp, ordered=F,  wrap= 45,text.size=5) +
         ggtitle("Section 1: General beliefs about climate change") + 
         theme(axis.text.y = element_text(hjust=0, size=16),
         axis.text.x = element_blank(),
@@ -21,7 +21,7 @@ source("./code/02_Likert.R")
         guides(fill=FALSE) 
 
 
-    Fig2b <- plot(likert_for_imp, ordered=F,  wrap=45, text.size=5) +
+    Fig1b <- plot(likert_for_imp, ordered=F,  wrap=45, text.size=5) +
         ggtitle("Section 2: Impacts of climate change on forest ecosystems") +
         theme(axis.text.y = element_text(hjust=0, size=16),
         axis.text.x =element_blank(),
@@ -30,7 +30,7 @@ source("./code/02_Likert.R")
         guides(fill=FALSE)
 
 
-    Fig2c <- plot(likert_current, ordered=F,  wrap=45,text.size=5) +
+    Fig1c <- plot(likert_current, ordered=F,  wrap=45,text.size=5) +
         ggtitle("Section 3: Current forest practices and need for adaptation") +
         theme(axis.text.y = element_text(hjust=0, size=16),
         axis.text.x = element_text(size=16),
@@ -39,7 +39,7 @@ source("./code/02_Likert.R")
         legend.text = element_text(size = 15)) +
         guides( fill=guide_legend("",nrow=1))    
 
-    pdf(file="./figs/Final/Figure_02.pdf", width=12.2, height=18)
+    pdf(file="./figs/Final/Figure_01.pdf", width=12.2, height=18)
         multiplot (Fig2a, Fig2b, Fig2c, cols=1)
     dev.off()
 
@@ -58,7 +58,10 @@ source("./code/02_Likert.R")
     colnames(fig2_stake$results) <- c("Group","Item", "Strongly\n disagree", "Disagree","Slightly\n disagree",
                                     "Neutral", "Slightly\n agree", "Agree", "Strongly\n agree")
   
-  
+    letters_fig2 <- mean_likert(survey,"Stakeholder", c(5,8,10,11,12,16,20,21)) %>%
+        arrange(Stakeholder)
+    
+    
     pdf(file="./figs/Final/Figure_02.pdf", width=9, height=16)
         plot(fig2_stake, ordered=F, text.size=4,plot.percents=T , wrap=160) +
             theme(axis.text.y = element_text(size = 14),
@@ -67,12 +70,18 @@ source("./code/02_Likert.R")
                 strip.background = element_rect (fill= "white", colour="white"),
                 legend.text = element_text(size = 12)) +
             guides(fill=guide_legend("",nrow=1)) +
-            annotate("text", x=6, y =-98, size=4, fontface=1,hjust=0,label=c("ab","a","abc","ab","ab","b","b","ab")) + #Federal
-            annotate("text", x=5, y =-98, size=4, fontface=1,hjust=0,label=c("b","a","ab","b","b","ab","b","bc")) + #Provincial
-            annotate("text", x=4, y =-98, size=4, fontface=1,hjust=0,label=c("c","b","a","c","c","ab","a","c")) + # Industry
-            annotate("text", x=3, y =-98, size=4, fontface=1,hjust=0,label=c("ab","a","abc","ab","ab","ab","b","abc")) + # Private
-            annotate("text", x=2, y =-98, size=4, fontface=1,hjust=0,label=c("a","a","bc","a","a","ab","b","ab")) + # Academia
-            annotate("text", x=1, y =-98, size=4, fontface=1,hjust=0,label=c("a","a","c","a","a","a","b","a")) # Students
+            annotate("text", x=6, y =-98, size=4, fontface=1,hjust=0,
+                     label = unlist(letters_fig2[1,-1])) + #Federal
+            annotate("text", x=5, y =-98, size=4, fontface=1,hjust=0,
+                     label = unlist(letters_fig2[2,-1])) + #Provincial
+            annotate("text", x=4, y =-98, size=4, fontface=1,hjust=0,
+                     label = unlist(letters_fig2[3,-1])) + # Industry
+            annotate("text", x=3, y =-98, size=4, fontface=1,hjust=0,
+                     label = unlist(letters_fig2[4,-1])) + # Private
+            annotate("text", x=2, y =-98, size=4, fontface=1,hjust=0,
+                     label = unlist(letters_fig2[5,-1])) + # Academia
+            annotate("text", x=1, y =-98, size=4, fontface=1,hjust=0,
+                     label =unlist(letters_fig2[6,-1])) # Students
         dev.off()
   
 
